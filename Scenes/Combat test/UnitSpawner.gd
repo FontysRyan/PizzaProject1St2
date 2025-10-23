859526c5f75ba68165a3ae4dd442f779f13aa44b
galactorsus( <504972@vistacollege.nl>
2025-10-23 13:26:22 +0200

    Add faction and unit type support to unit spawning
    Introduced faction assignment to unit spawners and units, allowing differentiation between FRIENDLY and ENEMY factions. Added support for specifying unit type, enabling dynamic loading of unit stats based on type. Updated test scene to include a second node for enemy units and adjusted animation setup in Unit.tscn.

--- Scenes/Combat test/UnitSpawner.gd
+++ Scenes/Combat test/UnitSpawner.gd
@@ -1,6 +1,8 @@
 extends Node
 
 @export var unit_scene: PackedScene
+enum Marker_Faction { FRIENDLY = 1, ENEMY = 2 }
+@export var faction: Marker_Faction
 func _ready():
 	var spawn_points = get_children()
 	for marker in spawn_points:
@@ -8,5 +10,7 @@ func _ready():
 			print(marker.name, marker.global_position)
 			var unit = unit_scene.instantiate()
 			unit.global_position = marker.global_position
+			unit.faction = faction    # set faction
+			unit.unit_type = "Archer"           # set unit type
 			add_child(unit)
 		
--- Scenes/Combat test/test_unit.gd
+++ Scenes/Combat test/test_unit.gd
@@ -3,13 +3,21 @@ extends Node2D
 enum Faction { FRIENDLY = 1, ENEMY = 2 }
 @export var faction: Faction = Faction.FRIENDLY
 @export var stats: UnitStats #script
-#stats is empty i need to load the unit stat into it
+@export var unit_type: String = "Archer"
+
 
 
 func _ready():
+	# Load stats if none assigned
 	if stats == null:
-		stats = load("res://Resources/units/Archer.tres") as UnitStats
-	print(stats.description)
+		var path = "res://Resources/units/%s.tres" % unit_type
+		var res = load(path)
+		if res and res is UnitStats:
+			stats = res
+		else:
+			push_warning("UnitStats resource not found at %s" % path)
+
+	print(stats.description if stats else "No stats loaded")
 	var sprite: AnimatedSprite2D = $AnimatedSprite2D
 	var ring1: Sprite2D = $faction_Ring1
 	var ring2: Sprite2D = $faction_Ring2
