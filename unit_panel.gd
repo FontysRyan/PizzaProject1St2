ff9b62177aff4257503e041c89c7149e2ad4cc1d
TheNiceLynX <147400278+TheNiceLynX@users.noreply.github.com>
2025-10-29 13:00:48 +0100

    Implement unit panel UI with level and price display
    Refactored unit_panel.gd to initialize and update UI elements for unit level, price, and texture. Updated unit_panel.tscn to add labels for level and price, and a coin icon. Adjusted BuildScreen (UI).tscn to fix node ordering and style resource placement.

--- unit_panel.gd
+++ unit_panel.gd
@@ -1,11 +1,19 @@
-#extends Panel
-#
-#@export var panel: UnitPanel
-## Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#get_parent().get_node("TextureRect")
-#
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
+extends Panel
+
+@export var panel: UnitPanel
+var unit_level: int = 1
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var color = panel.rarity.color
+	add_theme_color_override("UnitPanel", color)
+	var texture = panel.texture
+	get_parent().get_node("SpriteTexture").Texture = texture
+	var price = panel.rarity.cost
+	get_parent().get_node("PriceLabel").Text = price
+	var level = "Lvl: " + str(unit_level)
+	get_parent().get_node("LevelLabel").Text = level
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
