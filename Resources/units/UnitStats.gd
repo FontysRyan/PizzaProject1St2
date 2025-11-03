d8ac7f4921a1d32266ec48bff79704689a3801e3
Cumar003 <551113@student.fontys.nl>
2025-11-03 17:00:07 +0100

    update spriteframes

--- Resources/units/UnitStats.gd
+++ Resources/units/UnitStats.gd
@@ -3,7 +3,12 @@ extends Resource
 
 #Character sprite
 @export var texture: Texture2D
+<<<<<<< Updated upstream
 @export var sprite_frame: SpriteFrames
+=======
+@export var sprite_frames: SpriteFrames
+
+>>>>>>> Stashed changes
 #Stats
 @export var type: String = ""
 @export var description: String = ""
