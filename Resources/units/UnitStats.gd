d67512ec775ddba24324ae91f3a3858e4cf3e5e2
Cumar003 <551113@student.fontys.nl>
2025-11-03 18:12:17 +0100

    add anim
    added animation to the resource unit spriteframe

--- Resources/units/UnitStats.gd
+++ Resources/units/UnitStats.gd
@@ -3,7 +3,7 @@ extends Resource
 
 #Character sprite
 @export var texture: Texture2D
-
+@export var sprite_frames: SpriteFrames
 #Stats
 @export var type: String = ""
 @export var description: String = ""
