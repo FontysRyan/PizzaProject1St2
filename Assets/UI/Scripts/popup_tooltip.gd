b353330a9c8e160c9fd31bcabf98e55803f96106
Ryan <547558@student.fontys.nl>
2025-11-11 15:24:56 +0100

    Enhance tooltip system and rarity effects
    Refactored tooltip instancing to unit_panel.tscn and removed it from BuildScreen (UI).tscn. Updated Popup Tooltip.tscn to use a RichTextLabel for rarity with BBCode effects and added visibility control. Extended popup_tooltip.gd with toggle, opacity tweening, and rarity-based text effects. Improved FightButton appearance and set SavingIconContainer to hidden by default.

--- Assets/UI/Scripts/popup_tooltip.gd
+++ Assets/UI/Scripts/popup_tooltip.gd
@@ -1,4 +1,8 @@
 extends Control
+
+var opacity_tween: Tween = null
+@onready var rich_text = $BackgroundContainer/VBoxContainer/RichTextRarityLabel
+
 func _input(event: InputEvent) -> void:
 	if not visible or not (event is InputEventMouseMotion):
 		return
@@ -21,3 +25,49 @@ func _input(event: InputEvent) -> void:
 
 
 	global_position = pos
+
+
+func toggle (on: bool):
+	if on:
+		print("Showing tooltip")
+		show()
+		modulate.a = 0.0
+		tween_opacity(1.0)
+		set_text_effect_rarity(rich_text.text)
+	else:
+		modulate.a = 1.0
+		await tween_opacity(0.0).finished
+		hide()
+
+func tween_opacity(to: float):
+	if opacity_tween: opacity_tween.kill()
+	opacity_tween = create_tween()
+	opacity_tween.tween_property(self, "modulate:a", to, 0.3)
+	return opacity_tween
+
+
+
+
+func set_text_effect_rarity(rarity: String):
+	if not rich_text:
+		print("RichTextLabel not found!")
+		return
+
+	match rarity.strip_edges().capitalize():
+		"Common":
+			rich_text.bbcode_text = "[wave amp=10 freq=2][color=gray]Common[/color][/wave]"  # subtle wave effect
+		"Uncommon":
+			rich_text.bbcode_text = "[wave amp=15 freq=3][color=#00FF00]Uncommon[/color][/wave]"  # green with a stronger wave
+		"Rare":
+			rich_text.bbcode_text = "[wave amp=20 freq=4][color=#0000FF]Rare[/color][/wave]"  # blue with a noticeable wave
+		"Epic":
+			rich_text.bbcode_text = "[shake amp=5 freq=10][color=orange]Epic[/color][/shake]"  # orange with a shaking effect
+		"Legendary":
+			rich_text.bbcode_text = "[rainbow freq=0.5][shake amp=10 freq=15]Legendary[/shake][/rainbow]"  # rainbow color with a strong shake
+		_:
+			rich_text.text = rarity
+
+
+			
+func _ready():
+	set_text_effect_rarity(rich_text.text)
