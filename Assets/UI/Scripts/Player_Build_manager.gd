7c88d05e65dba7a19eabac8d787c40c109320f27
TheNiceLynX <147400278+TheNiceLynX@users.noreply.github.com>
2025-11-13 16:29:16 +0100

    idk what is happening

--- Assets/UI/Scripts/Player_Build_manager.gd
+++ Assets/UI/Scripts/Player_Build_manager.gd
@@ -31,47 +31,28 @@ func _ready():
 	assign_buffs_to_panels()
 
 func _process(delta):
-	var unit_names := []  # Collects all 9 slot resource names
-
 	for panel in get_children():
 		if panel is Panel:
-			var child_name: Unit_Panel = null
-
 			if panel.get_child_count() > 0:
-				var i = panel.get_child_count() - 1
-				var unit_tile = panel.get_child(i)
-				
-				if "panel" in unit_tile and unit_tile.panel:
-					var p = unit_tile.panel
-					#print("q" + p.name)
-					if p is UnitPanel:
-						# If it's a resource, get the file name
-						var temp_panel = panel_scene.instantiate()
-						temp_panel.premade_panel = unit_tile.premade_panel
-						temp_panel.unit_level = unit_tile.unit_level
-						temp_panel._fill_panel()
-						child_name = temp_panel
-						temp_panel.queue_free()
-					else:
-						child_name = null
+				var unit_tile = panel.get_child(0)
+				var slot_index = int(panel.name.replace("Panel_", ""))
+				var unit = GameController.get_unit_slot(slot_index)
+				if unit == null:
+					if "panel" in unit_tile and unit_tile.panel:
+						var child_name: Unit_Panel = null
+						var p = unit_tile.panel
+						if p is UnitPanel:
+							var temp_panel = panel_scene.instantiate()
+							print(temp_panel)
+							temp_panel.premade_panel = unit_tile.premade_panel
+							temp_panel.unit_level = unit_tile.unit_level
+							temp_panel._fill_panel()
+							child_name = temp_panel
+							GameController.update_build_slot(slot_index, child_name)
+						else:
+							GameController.clear_build_slot(slot_index)
 				else:
-					child_name = null
-
-			# Detect slot change
-			if child_name != null:
-				if build_slots.get(panel.name) != child_name.name:
-					build_slots[panel.name] = child_name.name
-				var slot_index = int(panel.name.replace("Panel_", ""))
-				GameController.update_build_slot(slot_index, child_name)
-			else:
-				var slot_index = int(panel.name.replace("Panel_", ""))
-				GameController.clear_build_slot(slot_index)
-
-			unit_names.append(child_name)
-
-	# Optional debugging snapshot
-	if unit_names.size() == 9:
-		pass
+					GameController.update_build_slot(slot_index, unit)
 
 
 
