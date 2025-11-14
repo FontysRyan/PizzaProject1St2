4bcc44863a7421f9ee891a14618f86acb0eec8e0
TheNiceLynX <147400278+TheNiceLynX@users.noreply.github.com>
2025-11-14 12:57:52 +0100

    Reapply "Add build clearing and unit slot getter functions"
    This reverts commit fcdfb95b2e1ed92a76b36c6436beca404afd707c.

--- game_controller.gd
+++ game_controller.gd
@@ -105,6 +105,7 @@ func begin_game():
 func clear_run_data():
 	Stats.running = false
 	Stats.units.clear()
+	clear_build()
 	Stats.gold = 0
 	Stats.round = 1
 	Stats.wave = 1
@@ -160,6 +161,29 @@ func clear_build_slot(slot_index: int) -> void:
 		8: unit_slot8 = null
 		9: unit_slot9 = null
 
+func get_unit_slot(slot_index: int) -> Unit_Panel:
+	match slot_index:
+		1: return unit_slot1
+		2: return unit_slot2
+		3: return unit_slot3
+		4: return unit_slot4
+		5: return unit_slot5
+		6: return unit_slot6
+		7: return unit_slot7
+		8: return unit_slot8
+		9: return unit_slot9
+		_: return null
+
+func clear_build() -> void:
+	unit_slot1 = null
+	unit_slot2 = null
+	unit_slot3 = null
+	unit_slot4 = null
+	unit_slot5 = null
+	unit_slot6 = null
+	unit_slot7 = null
+	unit_slot8 = null
+	unit_slot9 = null
 
 func set_game_speed(scale: float) -> void:
 	# Clamp to prevent negative or absurd values
